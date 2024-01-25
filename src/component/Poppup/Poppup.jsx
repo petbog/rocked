97e73c5107b4cffbae559265ea58ba1850b4e4f2
petrov bogdan <bogdan_emdetei_petrov@mail.ru>
2024-01-25 22:51:40 +0300

    style poppup

--- src/component/Poppup/Poppup.jsx
+++ src/component/Poppup/Poppup.jsx
@@ -1,19 +1,23 @@
 import s from './Poppup.module.css'
+import close from '../../img/Кнопка _Закрыть_.svg'
 
 const Poppup = ({ isActiv }) => {
     return (
         <div div className={isActiv ? `${s.background} + ${s.scrollOff}` : s.background} >
             <div className={s.poppup}>
-                <div className={s.info}>
-                    <div className={s.info_title}>Запишитесь на приём онлайн</div>
-                    <div className={s.info_text}>Администратор свяжется с вами через WhatsApp в течение дня и уточнит детали</div>
+                <div className={s.poppup_box}>
+                    <div className={s.info}>
+                        <div className={s.info_title}>Запишитесь<br/> на приём онлайн</div>
+                        <div className={s.info_text}>Администратор свяжется с вами через<br/> WhatsApp в течение дня и уточнит детали</div>
+                    </div>
+                    <div className={s.input}>
+                        <input className={s.input_name} type="text" placeholder='ФИО' required />
+                        <input className={s.input_tel} type="text" placeholder='Номер телефона' required />
+                        <input className={s.input_email} type="text" placeholder='Электронная почта' required />
+                        <button className={s.input_button}>Записаться</button>
+                    </div>
                 </div>
-                <div className={s.input}>
-                    <input className={s.input_name} type="text" placeholder='ФИО' required />
-                    <input className={s.input_tel} type="text" placeholder='Номер телефона' required />
-                    <input className={s.input_email} type="text" placeholder='Электронная почта' required />
-                    <button>Записаться</button>
-                </div>
+                <img className={s.closePoppup} src={close} alt="close" />
             </div>
         </div >
 
