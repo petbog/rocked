7f408cf0d538f8d391349785c2499441aebab157
Petrov Bogdan <bogdan_emdetei_petrov@mail.ru>
2024-02-08 02:07:26 +0300

    доделал слайдер

--- src/component/Slider/Slider.jsx
+++ src/component/Slider/Slider.jsx
@@ -4,27 +4,40 @@ import 'swiper/swiper-bundle.css';
 import { Pagination, Navigation, Autoplay } from 'swiper/modules';
 import MainBottom from './../MainBottom/MainBottom';
 import { SliderComponent } from '../SliderComponent/SliderComponent'
+import { useState } from 'react';
 
 const Slider = ({ PoppupWindow, setPoppupWindow }) => {
+    const [swiper, setSwiper] = useState(null);
+    const CustomNextArrow = () => (
+        <button className="custom-next-arrow" onClick={() => swiper.slideNext()}></button>
+    );
+    
+    const CustomPrevArrow = () => (
+        <button className="custom-prev-arrow" onClick={() => swiper.slidePrev()}></button>
+    );
 
 
 
     return (
         <div className='slider'>
             <Swiper
-                //  autoplay={{
-                //     delay: 2500,
-                //     disableOnInteraction: false,
-                // }}
+                 autoplay={{
+                    delay: 2500,
+                    disableOnInteraction: false,
+                }}
                 pagination={{
                     type: 'fraction',
                 }}
 
-                navigation={true}
+                navigation={{
+                    nextEl: '.custom-next-arrow',
+                    prevEl: '.custom-prev-arrow',
+                }}
+
                 modules={[Pagination, Navigation, Autoplay]}
                 className="mySwiper"
+                onSwiper={(swiper) => { setSwiper(swiper) }}
             >
-
                 {
                     SliderComponent.map((slide, i) => (
                         <SwiperSlide key={i}>
@@ -33,6 +46,8 @@ const Slider = ({ PoppupWindow, setPoppupWindow }) => {
                     ))
                 }
             </Swiper>
+            <CustomNextArrow />
+            <CustomPrevArrow />
         </div>
     )
 }
